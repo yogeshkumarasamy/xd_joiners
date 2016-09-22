1970728de39dcbedd49bd239d584d6b62ddf1ac5
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2016-09-22 14:43:53 +0530

    Login page & forget Password page integration Done

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,19 @@
-xdapp.controller("loginController",["$scope","$http",function($scope,$http){
+xdapp.controller("mainController", ["$scope", function ($scope) {
+    self = this;
+
+    self.authLogin = function () {
+        console.log(self.userName + self.userPassword);
+    }
 
 }])
+
+xdapp.controller("loginController", ["$scope", "$http", function ($scope, $http) {
+    self = this;
+   
+    self.authLogin = function () {
+        console.log(self.userName + self.userPassword);
+    }
+
+}])
+
+
--- js/route.js
+++ js/route.js
@@ -1,10 +1,14 @@
-xdapp.config(function($stateProvider,$urlRouterPriovider){
-
-  $urlRouterPriovider.otherwise('/');
+xdapp.config(function($stateProvider,$urlRouterProvider){
+    
+  $urlRouterProvider.otherwise('/login');
   $stateProvider
-    .state("login",{
-      url : "/login",
-      templateUrl : "/template/login.html"
-    })
+  .state('login', {
+      url: '/login',
+      templateUrl: "template/login.html"
+  })
+    .state('forgetPassword', {
+        url: "/forgetPassword",
+        templateUrl: "template/forget_Password.html"
+    });
 
 })
