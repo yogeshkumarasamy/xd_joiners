f17c14048245a59fb33a2e7dddf8ca02fd9bcc7a
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2016-09-22 16:08:59 +0530

    creating user with angular fire methods

--- js/controller.js
+++ js/controller.js
@@ -1,36 +1,20 @@
-var XdBase = new Firebase("https://xdnewjoiner.firebaseio.com/");
-var xdBase = $firebaseObject(XdBase);
+xdapp.controller("loginController", ["$scope", "$http", function($scope, $http) {
 
-xdapp.controller("mainController", ["$scope", "$firebaseObject", function ($scope, $firebaseObject) {
-  
-    console.log($scope.xdBase);
-    self = this;
-
-    self.authLogin = function () {
-        console.log(self.userName + self.userPassword);
+}]);
+xdapp.controller('RegistrationController', ['$scope', '$firebaseObject', '$firebaseAuth', function($scope, $firebaseObject, $firebaseAuth) {
+    $scope.msg = "I am in RegistrationController";
+    var ref = new Firebase("https://xdjoiners.firebaseio.com/");
+    $scope.authObj = $firebaseAuth(ref);
+    $scope.saveUser = function() {
+    	console.log($scope.authObj);
+        $scope.authObj.$createUser({
+            email: $scope.user.name,
+            password: "mypassword"
+        }).then(function(userData) {
+            console.log(userData);
+        })
+        .catch(function(error) {
+            console.error("Error: ", error);
+        });
     }
-
 }])
-
-xdapp.controller("loginController", ["$scope", "$http", function ($scope, $http) {
-    self = this;
-   
-    self.authLogin = function () {
-        console.log(self.userName + self.userPassword);
-    }
-
-}])
-
-xdapp.controller("registerController", ["$scope", "$http", function ($scope, $http) {
-    self = this;
-
-    self.regUser = function () {
-        console.log(self.userName);
-
-        xdBase.$bindTo($scope, "self.userNam");
-    }
-
-}])
-
-
-
--- js/route.js
+++ js/route.js
@@ -1,19 +1,14 @@
-xdapp.config(function($stateProvider,$urlRouterProvider){
-    
-  $urlRouterProvider.otherwise('/login');
-  $stateProvider
-  .state('login', {
-      url: '/login',
-      templateUrl: "template/login.html"
-  })
-      .state('register', {
-          url: "/register",
-          templateUrl: "template/register.html"
-      })
-
-    .state('forgetPassword', {
-        url: "/forgetPassword",
-        templateUrl: "template/forget_Password.html"
-    });
-
-})
+xdapp.config(function($stateProvider, $urlRouterProvider) {    
+    $urlRouterProvider.otherwise('/');    
+    $stateProvider        
+        // Login View
+        .state('login', {
+            url: '/login',
+            templateUrl: 'template/login.html'
+        }).state('register', {
+            url: '/register',
+            templateUrl: 'template/register.html',
+            controller: 'RegistrationController'
+        });
+        
+});
